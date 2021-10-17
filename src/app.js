0ab9353ff6f5c05bacd2b8a924ab87bcf63ca548
zahra-p <zahra.adelinia@gmail.com>
2021-10-17 19:38:48 +0330

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
+
+function formatDate(timestamp){  //میلی ثانیه میگیره
+    let date = new Date(timestamp);
+    let hours= date.getHours();
+    if (hours < 10){
+        hours= `0{hours}`;
+    }
+
+    let minutes=date.getMinutes();
+    if (minutes < 10){
+        minutes= `0{minutes}`;
+    }
+    
+    let days=["Saturday","Monday","Tuesday","Wednesday","Thursday","Friday"];
+    let day=days[date.getDay()];
+    
+    return `${day} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement=document.querySelector("#temperature");
@@ -5,12 +25,15 @@ function displayTemperature(response){
     let descriptionElement=document.querySelector("#description");
     let humidityElement=document.querySelector("#humidity");
     let speedElement=document.querySelector("#speed");
+    let dateElement=document.querySelector("#date");
 
     temperatureElement.innerHTML=Math.round(response.data.list[0].main.temp);
     cityElement.innerHTML=response.data.list[0].name;
     descriptionElement.innerHTML=response.data.list[0].weather[0].description;
     humidityElement.innerHTML=response.data.list[0].main.humidity;
     speedElement.innerHTML=Math.round(response.data.list[0].wind.speed);
+    dateElement.innerHTML=formatDate(response.data.list[0].dt * 1000);
+    console.log(response.data.list[0].dt);
 }
 
 
